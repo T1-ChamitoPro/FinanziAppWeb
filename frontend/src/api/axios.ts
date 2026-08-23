6dde246762fa416e32ec4457cbc1dc982e3b3134
Francisco <franciscosampayo2503@gmail.com>
2026-08-22 21:28:26 -0500

    falto el /api en la url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://finanziappweb.onrender.com',
+  baseURL: 'https://finanziappweb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
